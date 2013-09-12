8d0d217b504d95e6e08171afbd6ce95b2f999fff
Ryota Morita <ryota.morita@gmail.com>
2013-09-12 16:20:40 +0900

    タッチで船が動く

---
+++ js/GameScene.js
@@ -0,0 +1,50 @@
+
+var gameScene = function (game) {
+
+    var scene = new Scene ();
+    scene.backgroundColor = 'rgba(128, 128, 256, 1)';
+
+    var friendlyShip = new Sprite (79, 82);
+    friendlyShip.image = game.assets['img/friendly_ship.png'];
+    friendlyShip.x = 100;
+    friendlyShip.y = 100;
+    friendlyShip.npos = new Vec2(friendlyShip.x, friendlyShip.y);
+    friendlyShip.pos = new Vec2(friendlyShip.x + 1, friendlyShip.y + 1);
+    friendlyShip.dvec = (new Vec2(1, 0)).normalize();
+
+    var touchque = new Array();
+    var touchstart = {x:0, y:0};
+    scene.addEventListener('touchstart', function(e) {
+        touchstart.x = e.localX;
+        touchstart.y = e.localY;
+    });
+
+    scene.addEventListener('touchmove', function(e) {
+        var lc = touchstart.x > friendlyShip.x;
+        var rc = touchstart.x < (friendlyShip.x + friendlyShip.width);
+        var uc = touchstart.y > friendlyShip.y;
+        var dc = touchstart.y < friendlyShip.y + friendlyShip.height;
+        if (lc && rc && uc && dc) {
+            touchque.push(new Vec2(e.localX, e.localY));
+        }
+    });
+
+    scene.addEventListener('enterframe', function () {
+        var tmp = friendlyShip.npos.subV(friendlyShip.pos);
+        if (tmp.length() > 1) {
+            tmp.normalize();
+            friendlyShip.pos = friendlyShip.pos.addV(tmp);
+
+            friendlyShip.x = friendlyShip.pos.x - (friendlyShip.width / 2);
+            friendlyShip.y = friendlyShip.pos.y - (friendlyShip.height / 2);
+        }
+        else if (touchque.length > 0) {
+            friendlyShip.npos = touchque.shift();
+        }
+
+    });
+
+    scene.addChild(friendlyShip);
+
+    return scene;
+};
--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-
-enchant();
-
 window.onload = function () {
-    var game = new Game (320, 480);
-
+    var game = new Game (960, 640);
+    game.fps = 30;
+    // load image.
+    game.preload(
+        'img/bullet.png',
+        'img/enemy.png',
+        'img/friendly_ship.png'
+    );
     game.onload = function () {
+        game.replaceScene(gameScene(game));
     };
 
     game.start();
 };
+
